f2a29e0a4b545b96e9d11080434627ae9ed10909
BrandenXia <xxtbranden@outlook.com>
2024-07-31 14:47:26 +0800

    perf(docs): enhance deploy for vercel

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sitemap()]
+  site: "https://oceancleanx.org",
+  integrations: [tailwind(), react(), sitemap()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enable: true },
+  }),
+  redirects: {
+    "/": "/docs",
+    "/docs": "/docs/intro",
+  },
 });
